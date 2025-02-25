5d7d4c79ffaeba28c60fd15da4040829c3ee276d
marouane-sadoune <marouanesadoune@gmail.com>
2025-02-25 00:11:27 +0000

    importe of factorys

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,7 +4,13 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use App\Factory\EnsiegnantFactoryFactory;
+use App\Factory\EtudiantFactory;
+use App\Factory\FilierFactory;
+use App\Factory\ModuleFactory;
+use App\Factory\NoteFactory;
+use App\Factory\aaa; #Semester
+use App\Factory\no; #user
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
